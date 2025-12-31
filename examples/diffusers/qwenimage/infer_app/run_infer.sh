b39a8ab240395b926923f088b47c6f2696f04877
Dong1017 <xwdong1998@163.com>
2025-12-31 06:26:03 +0000

    update sh

--- examples/diffusers/qwenimage/infer_app/run_infer.sh
+++ examples/diffusers/qwenimage/infer_app/run_infer.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-export ASCEND_RT_VISIBLE_DEVICES=6,7
+export ASCEND_RT_VISIBLE_DEVICES=0,1
 
 # Distributed training configuration
 MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1"}
 MASTER_PORT=${MASTER_PORT:-$(shuf -i 20001-29999 -n 1)}
 NPROC_PER_NODE=${WORLD_SIZE:-2}
 
-entry_file="test_qwenimage.py"
+entry_file="example_inference.py"
 
 model_id="Qwen-Image"
 
@@ -14,7 +14,7 @@ msrun --worker_num=${NPROC_PER_NODE} \
     --local_worker_num=${NPROC_PER_NODE} \
     --master_addr=${MASTER_ADDR} \
     --master_port=${MASTER_PORT} \
-    --log_dir="logs/multinpu_qwen" \
+    --log_dir="logs/infer" \
     --join=True \
     ${entry_file} \
     --model_id "${model_id}"
